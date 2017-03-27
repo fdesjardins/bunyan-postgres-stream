59aa67d4735cad52b9f6cefe7fcb84ab56d80347
Forrest Desjardins <desjardinsfg@gmail.com>
2017-03-26 21:34:20 -0800

    replace xo with eslint + standard config

--- index.js
+++ index.js
@@ -1,80 +1,78 @@
-'use strict';
-
-const Writable = require('stream').Writable;
-const pg = require('pg');
+const Writable = require('stream').Writable
+const pg = require('pg')
 
 class LogStream extends Writable {
-	constructor(options) {
-		super(options);
+  constructor (options) {
+    super(options)
 
-		if (options.connection === undefined || options.tableName === undefined) {
-			throw new Error('Invalid bunyan-postgres stream configuration');
-		}
+    if (options.connection === undefined || options.tableName === undefined) {
+      throw new Error('Invalid bunyan-postgres stream configuration')
+    }
 
-		if (options.connection.client && options.connection.client.makeKnex) {
-			this.knex = options.connection;
-			this._write = this._writeKnex;
-		}
+    if (options.connection.client && options.connection.client.makeKnex) {
+      this.knex = options.connection
+      this._write = this._writeKnex
+    }
 
-		if (typeof options.connection === 'object') {
-			this.connection = options.connection;
-			this._write = this._writePgPool;
-			this.pool = new pg.Pool(this.connection);
-		}
+    if (typeof options.connection === 'object') {
+      this.connection = options.connection
+      this._write = this._writePgPool
+      this.pool = new pg.Pool(this.connection)
+    }
 
-		this.tableName = options.tableName;
-	}
+    this.tableName = options.tableName
+  }
 
-	_writeKnex(chunk, env, cb) {
-		const content = JSON.parse(chunk.toString());
-		this.knex
-			.insert({
-				name: content.name,
-				level: content.level,
-				hostname: content.hostname,
-				msg: content.msg,
-				pid: content.pid,
-				time: content.time,
-				content: JSON.stringify(content)
-			})
-			.into(this.tableName)
-			.asCallback(cb);
-	}
+  _writeKnex (chunk, env, cb) {
+    const content = JSON.parse(chunk.toString())
+    this.knex.insert({
+      name: content.name,
+      level: content.level,
+      hostname: content.hostname,
+      msg: content.msg,
+      pid: content.pid,
+      time: content.time,
+      content: JSON.stringify(content)
+    })
+    .into(this.tableName)
+    .asCallback(cb)
+  }
 
-	writePgPool(client, content) {
-		return client.query(`
-			insert into ${this.tableName}
-				(name, level, hostname, msg, pid, time, content)
-			values (
-				'${content.name}',
-				'${content.level}',
-				'${content.hostname}',
-				'${content.msg}',
-				'${content.pid}',
-				'${content.time}',
-				'${JSON.stringify(content)}'
-			)`);
-	}
+  writePgPool (client, content) {
+    return client.query(`
+      insert into ${this.tableName}
+        (name, level, hostname, msg, pid, time, content)
+      values (
+        '${content.name}',
+        '${content.level}',
+        '${content.hostname}',
+        '${content.msg}',
+        '${content.pid}',
+        '${content.time}',
+        '${JSON.stringify(content)}'
+      );
+    `)
+  }
 
-	_writePgPool(chunk, env, cb) {
-		const content = JSON.parse(chunk.toString());
-		this.pool.connect().then(client => {
-			return this.writePgPool(client, content).then(result => {
-				cb(null, result.rows);
-				client.release();
-			});
-		})
-		.catch(err => cb(err));
-	}
+  _writePgPool (chunk, env, cb) {
+    const content = JSON.parse(chunk.toString())
+    this.pool.connect().then(client => {
+      return this.writePgPool(client, content).then(result => {
+        cb(null, result.rows)
+        client.release()
+      })
+    })
+    .catch(err => cb(err))
+  }
 
-	end(cb) {
-		if (this.pool) {
-			return this.pool.end(cb);
-		}
-		cb();
-	}
+  end (cb) {
+    if (this.pool) {
+      return this.pool.end(cb)
+    }
+    cb()
+  }
 }
 
 module.exports = (options = {}) => {
-	return new LogStream(options);
-};
+  return new LogStream(options)
+}
