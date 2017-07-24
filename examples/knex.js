0d24c4b684baaa09ab290dbd427ff2fcfa5455ad
Forrest Desjardins <desjardinsfg@gmail.com>
2017-07-24 13:38:11 -0800

    eslint and tweak examples

--- examples/knex.js
+++ examples/knex.js
@@ -1,26 +1,28 @@
-const bunyan = require('bunyan');
-const bunyanPostgresStream = require('./');
+const bunyan = require('bunyan')
+const bunyanPostgresStream = require('../')
 const knex = require('knex')
 
 const db = knex({
-	client: 'pg',
-	connection: {
-		host: 'localhost',
-		user: 'postgres',
-		password: 'password',
-		database: 'db'
-	}
+  client: 'pg',
+  connection: {
+    host: 'localhost',
+    user: 'postgres',
+    password: 'password',
+    database: 'db'
+  }
 })
 
 const stream = bunyanPostgresStream({
-	connection: db,
-	tableName: 'logs'
-});
+  connection: db,
+  tableName: 'logs'
+})
 
 const log = bunyan.createLogger({
-	name: 'postgres stream',
-	level: 'info',
-	stream
-});
+  name: 'postgres stream',
+  level: 'info',
+  stream
+})
 
-log.info('something happened');
+log.info('something happened')
+
+// dispose of knex as usual in your program
--- examples/simple.js
+++ examples/simple.js
@@ -1,23 +1,23 @@
-const bunyan = require('bunyan');
-const bunyanPostgresStream = require('./');
+const bunyan = require('bunyan')
+const bunyanPostgresStream = require('../')
 
 const stream = bunyanPostgresStream({
-	connection: {
-		host: 'localhost',
-		user: 'postgres',
-		password: 'password',
-		database: 'db'
-	},
-	tableName: 'logs'
-});
+  connection: {
+    host: 'localhost',
+    user: 'postgres',
+    password: 'password',
+    database: 'db'
+  },
+  tableName: 'logs'
+})
 
 const log = bunyan.createLogger({
-	name: 'postgres stream',
-	level: 'info',
-	stream
-});
+  name: 'postgres stream',
+  level: 'info',
+  stream
+})
 
-log.info('something happened');
+log.info('something happened')
 
 // explicity dispose of the database connection pool
-stream.end();
+stream.end()
