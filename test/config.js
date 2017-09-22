4d5652eb757fa99f5ed45753bf7878bc51f7f5ef
Forrest Desjardins <desjardinsfg@gmail.com>
2017-09-21 23:16:07 -0800

    replace eslint dependencies with standard

--- test/config.js
+++ test/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-	client: 'pg',
-	connection: {
-		adapter: 'postgresql',
-		database: 'travis_ci_test',
-		user: 'postgres'
-	}
-};
+  client: 'pg',
+  connection: {
+    adapter: 'postgresql',
+    database: 'travis_ci_test',
+    user: 'postgres'
+  }
+}
