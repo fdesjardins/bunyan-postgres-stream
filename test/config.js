c2d1a78403a81d0e0fd6aec6106ab6e3bd48baf5
Forrest Desjardins <desjardinsfg@gmail.com>
2019-05-05 18:24:41 -0800

    remove host port mapping in docker-compose

--- test/config.js
+++ test/config.js
@@ -3,6 +3,7 @@ module.exports = {
   connection: {
     adapter: 'postgresql',
     host: process.env.POSTGRES_HOST || 'localhost',
+    port: process.env.POSTGRES_PORT || '5432',
     database: process.env.POSTGRES_DB || 'test',
     user: process.env.POSTGRES_USER || 'postgres',
     password: process.env.POSTGRES_PASSWORD || 'password'
